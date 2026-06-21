49327b21c7b298d6b232bea1a61af830d497575b
shamim4010 <shamimislam401017@gmail.com>
2026-06-22 03:28:07 +0600

    Implement API endpoints for retrieving classes and bookings

--- index.js
+++ index.js
@@ -28,8 +28,29 @@ async function run() {
         const classes = db.collection('classes');
         const bookings = db.collection('bookings');
 
-        app.get('classes', (req, res) => {
-            
+        app.get('/all-classes', async (req, res) => {
+            const cursor = classes.find();
+            const results = await cursor.toArray();
+            res.send(results);
+        })
+
+        app.get('/all-classes/:id', async (req, res) => {
+            const { id } = req.params
+            const query = { _id: new ObjectId(id) };
+            const results = await classes.findOne(query);
+            res.send(results);
+        })
+
+        app.get('/booking/:userId', async (req, res) => {
+            const { userId } = req.params
+            const results = await bookings.find({ userId: userId }).toArray();
+            res.json(results);
+        })
+
+        app.post('/booking', async (req, res) => {
+            const bookingClasses = req.body
+            const result = await bookings.insertOne(bookingClasses).toArray();
+            res.json(result);
         })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
