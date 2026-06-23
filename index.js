dbf77361bd350ef0a8bfb013da17d2d3afbc9e35
shamim4010 <shamimislam401017@gmail.com>
2026-06-23 12:32:13 -0600

    Fix express.json middleware usage and add Vercel configuration file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const app = express()
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
